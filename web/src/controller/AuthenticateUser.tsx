ffbd1fe21c20048f568c35a570c3523a54377115
Killi-Poyi <dheerusmenon@gmail.com>
2025-10-17 16:37:45 +0530

    feat: (D)Shared Instance for prisma DEBUGING

--- web/src/controller/AuthenticateUser.tsx
+++ web/src/controller/AuthenticateUser.tsx
@@ -1,11 +1,11 @@
 import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
+
 import { User } from "@supabase/supabase-js";
 
 
 export default async function AuthenticateUser(): Promise<User> {
-  const cookieStore = cookies();
-  const supabase = await createClient(cookieStore);
+
+  const supabase =  await createClient();
 
   const { data: { user } } = await supabase.auth.getUser();
 
